ddd202fa11f040682b494792aa05e32ff0bf6544
William Maldonado <william.marcy08@gmail.com>
2024-01-20 02:00:01 -0600

    Eliminar bin y debug

--- AuthenticationExample/AuthenticationExampleApi/obj/Debug/net8.0/AuthenticationExampleApi.AssemblyInfo.cs
+++ AuthenticationExample/AuthenticationExampleApi/obj/Debug/net8.0/AuthenticationExampleApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthenticationExampleApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2799cccfa8fdd0c0fef41eec48b999ea2caaecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthenticationExampleApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthenticationExampleApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
